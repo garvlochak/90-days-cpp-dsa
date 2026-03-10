111f6f1970fa1a0790c85379b4f9c2d6e45bbcd3
Garv Lochak <garvlochak@Garvs-MacBook-Pro.local>
2026-03-10 11:18:28 +0530

    Day 1: C++ basics, IO, variables, arithmetic

---
+++ days/day01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Program 1:Sum of two numbers 
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+
+    //Program 2: Square of a number
+    int n;
+    cin>>n;
+    cout<<n*n<<endl;
+
+
+    //Program 3:Triple of a number
+    int c;
+    cin>>c;
+    cout<<c*3<<endl;
+
+
+    //Area of a rectangle
+    double length,breadth;
+    cin>>length>>breadth;
+    cout<<length*breadth<<endl;
+
+}
