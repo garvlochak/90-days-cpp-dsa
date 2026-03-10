ca3e182ef0c635c9bd949a685c267fdf3bf05e32
Garv Lochak <garvlochak.dev@gmail.com>
2026-03-10 11:30:07 +0530

    Update day01.cpp

--- days/day01.cpp
+++ days/day01.cpp
@@ -5,23 +5,23 @@ int main() {
     //Program 1:Sum of two numbers 
     int a,b;
     cin >> a >> b;
-    cout << a+b << endl;
+    cout << a + b << endl;
 
     //Program 2: Square of a number
     int n;
     cin >> n;
-    cout << n*n << endl;
+    cout << n * n << endl;
 
 
     //Program 3:Triple of a number
     int c;
     cin >> c;
-    cout << c*3 << endl;
+    cout << c * 3 << endl;
 
 
     //Program 4:Area of a rectangle
     double length,breadth;
     cin >> length >> breadth;
-    cout << length*breadth << endl;
+    cout << length * breadth << endl;
 
 }
